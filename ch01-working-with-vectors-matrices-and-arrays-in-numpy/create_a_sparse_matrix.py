a9b32604b85c91ac6be0299b803b25e80ed57ae9
tlab <plaza-homing-7e@icloud.com>
2026-02-05 08:41:06 +1100

    Ruff format

--- ch01-working-with-vectors-matrices-and-arrays-in-numpy/create_a_sparse_matrix.py
+++ ch01-working-with-vectors-matrices-and-arrays-in-numpy/create_a_sparse_matrix.py
@@ -7,9 +7,9 @@ matrix = np.array([[0, 0], [0, 1], [3, 0]])
 
 matrix_sparse = sparse.csr_matrix(matrix)
 
-matrix_large = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [3, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+matrix_large = np.array(
+    [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [3, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+)
 
 matrix_large_sparse = sparse.csr_matrix(matrix_large)
 
